78bc5f6aaca1b213675e16e75d36a65711f53dda
mtums <mikatums@iCloud.com>
2023-06-18 15:24:11 -0400

    Finished BujoModel implementation.

--- src/main/java/cs3500/pa05/model/BujoModel.java
+++ src/main/java/cs3500/pa05/model/BujoModel.java
@@ -1,7 +1,7 @@
 package cs3500.pa05.model;
 
 public interface BujoModel {
-  void changeTheme();
+  void changeTheme(Theme theme);
   void addTask(Task task);
   void addEvent(Event event);
 }
--- src/main/java/cs3500/pa05/model/BujoModelImpl.java
+++ src/main/java/cs3500/pa05/model/BujoModelImpl.java
@@ -7,22 +7,25 @@ public class BujoModelImpl implements BujoModel {
   List<Day> days;
   List<Event> allEvents;
   List<Task> allTasks;
+  Theme theme;
 
   public BujoModelImpl() {
     this.days = new ArrayList<>();
     this.allEvents = new ArrayList<>();
     this.allTasks = new ArrayList<>();
+    this.theme = null;
   }
 
-  public BujoModelImpl(List<Day> days, List<Event> allEvents, List<Task> allTasks) {
+  public BujoModelImpl(List<Day> days, List<Event> allEvents, List<Task> allTasks, Theme theme) {
     this.days = days;
     this.allEvents = allEvents;
     this.allTasks = allTasks;
+    this.theme = theme;
   }
 
   @Override
-  public void changeTheme() {
-
+  public void changeTheme(Theme theme) {
+    this.theme = theme;
   }
 
   @Override
@@ -36,7 +39,24 @@ public class BujoModelImpl implements BujoModel {
   }
 
   public void createDays() {
+    List<Day> days = new ArrayList<>();
+    Day sunday = new Day(new ArrayList<>(), new ArrayList<>(), 1, 1);
+    Day monday = new Day(new ArrayList<>(), new ArrayList<>(), 1, 1);
+    Day tuesday = new Day(new ArrayList<>(), new ArrayList<>(), 1, 1);
+    Day wednesday = new Day(new ArrayList<>(), new ArrayList<>(), 1, 1);
+    Day thursday = new Day(new ArrayList<>(), new ArrayList<>(), 1, 1);
+    Day friday = new Day(new ArrayList<>(), new ArrayList<>(), 1, 1);
+    Day saturday = new Day(new ArrayList<>(), new ArrayList<>(), 1, 1);
 
+    days.add(sunday);
+    days.add(monday);
+    days.add(tuesday);
+    days.add(wednesday);
+    days.add(thursday);
+    days.add(friday);
+    days.add(saturday);
+
+    this.days = days;
   }
 
   public List<Task> getTasks() {
