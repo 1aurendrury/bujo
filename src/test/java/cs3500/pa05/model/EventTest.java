47ce5449275514d2ac9a50fc45be4c2f91a3fdc1
mtums <mikatums@iCloud.com>
2023-06-21 14:37:46 -0400

    Added all tests for model and began adding JavaDoc for model.

--- src/test/java/cs3500/pa05/model/EventTest.java
+++ src/test/java/cs3500/pa05/model/EventTest.java
@@ -8,6 +8,29 @@ class EventTest {
   //test if invalid startTime is entered
   @Test
   void testInvalidEntry() {
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+      new Event("TestEvent", "this is a test event", "Modnay", 0, 2.0);
+    });
+
+    assertEquals("Start time must be between 1 and 24, inclusive.", exception.getMessage());
+
+    IllegalArgumentException exception2 = assertThrows(IllegalArgumentException.class, () -> {
+      new Event("TestEvent", "this is a test event", "Modnay", 25, 2.0);
+    });
+
+    assertEquals("Start time must be between 1 and 24, inclusive.", exception.getMessage());
+  }
+
+  @Test
+  void testValidEntry() {
+    Event event = new Event("TestEvent", "this is a test event", "Monday", 1, 2.0);
+    assertEquals(1, event.startTime);
+
+    Event event2 = new Event("TestEvent", "this is a test event", "Monday", 10, 2.0);
+    assertEquals(10, event2.startTime);
+
+    Event event3 = new Event("TestEvent", "this is a test event", "Monday", 24, 2.0);
+    assertEquals(24, event3.startTime);
   }
 
 }
